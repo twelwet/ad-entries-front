35c4e57cd82946be53cf41c2a19784d5cef6fb0a
twelwet <akimovmax@mail.ru>
2021-08-21 16:28:31 +0300

    Поправил проп-тайпы для state.stat.data

--- src/prop-types/stat-data.prop.js
+++ src/prop-types/stat-data.prop.js
@@ -2,13 +2,15 @@ import PropTypes from 'prop-types';
 import entriesDataProp from './entries-data.prop';
 
 export default PropTypes.shape({
+  timeStamp: PropTypes.string,
   count: PropTypes.shape({
     all: PropTypes.number.isRequired,
     enabled: PropTypes.number.isRequired,
     disabled: PropTypes.number.isRequired,
     withEmails: PropTypes.number,
   }),
-  creation: PropTypes.array.isRequired,
-  lastLogon: PropTypes.array.isRequired,
+  creation: PropTypes.array,
+  lastLogon: PropTypes.array,
   topBoxes: entriesDataProp,
+  values: PropTypes.arrayOf(PropTypes.number),
 });
